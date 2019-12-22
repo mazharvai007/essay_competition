e873f7589f1a38f734abc45ea44075d82abaa7ae
swopnil <swopnil420@gmail.com>
2019-12-22 23:34:10 +0600

    Connect with DB

---
+++ includes/db.php
@@ -0,0 +1,30 @@
+<?php
+
+ob_start();
+
+/*
+ * ===========
+ * DB Connect
+ * ===========
+ */
+
+$db["db_host"] = "localhost";
+$db["db_user"] = "root";
+$db["db_pass"] = "91221";
+$db["db_name"] = "essay_competition";
+
+foreach ($db as $key => $value)
+{
+    define(strtoupper($key), $value);
+}
+
+$connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+$utf_query = "SET NAMES utf8";
+
+mysqli_query($connect, $utf_query);
+
+if (!$connect)
+{
+    echo "Database connection is failed!";
+}
